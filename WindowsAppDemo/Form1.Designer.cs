2e40768301fa99ed5d3f72479d207bb0d26c6543
Kenan Kurda <intecbrussel24@gmail.com>
2023-12-22 10:16:39 +0100

    Add project files.

---
+++ WindowsAppDemo/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WindowsAppDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadBooks = new System.Windows.Forms.Button();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtYearPublished = new System.Windows.Forms.TextBox();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            this.lblYearPublished = new System.Windows.Forms.Label();
+            this.lblGenre = new System.Windows.Forms.Label();
+            this.lstBooks = new System.Windows.Forms.ListBox();
+            this.btnAddBook = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoadBooks
+            // 
+            this.btnLoadBooks.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadBooks.Location = new System.Drawing.Point(125, 229);
+            this.btnLoadBooks.Name = "btnLoadBooks";
+            this.btnLoadBooks.Size = new System.Drawing.Size(163, 34);
+            this.btnLoadBooks.TabIndex = 0;
+            this.btnLoadBooks.Text = "Load Books";
+            this.btnLoadBooks.UseVisualStyleBackColor = true;
+            this.btnLoadBooks.Click += new System.EventHandler(this.btnLoadBooks_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(125, 122);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(307, 122);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(100, 20);
+            this.txtAuthor.TabIndex = 2;
+            // 
+            // txtYearPublished
+            // 
+            this.txtYearPublished.Location = new System.Drawing.Point(449, 122);
+            this.txtYearPublished.Name = "txtYearPublished";
+            this.txtYearPublished.Size = new System.Drawing.Size(100, 20);
+            this.txtYearPublished.TabIndex = 3;
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.Location = new System.Drawing.Point(662, 122);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(100, 20);
+            this.txtGenre.TabIndex = 4;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(149, 81);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(45, 22);
+            this.lblTitle.TabIndex = 5;
+            this.lblTitle.Text = "Title";
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.AutoSize = true;
+            this.lblAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAuthor.Location = new System.Drawing.Point(319, 81);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(63, 22);
+            this.lblAuthor.TabIndex = 6;
+            this.lblAuthor.Text = "Author";
+            // 
+            // lblYearPublished
+            // 
+            this.lblYearPublished.AutoSize = true;
+            this.lblYearPublished.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYearPublished.Location = new System.Drawing.Point(445, 81);
+            this.lblYearPublished.Name = "lblYearPublished";
+            this.lblYearPublished.Size = new System.Drawing.Size(132, 22);
+            this.lblYearPublished.TabIndex = 7;
+            this.lblYearPublished.Text = "Year Published";
+            // 
+            // lblGenre
+            // 
+            this.lblGenre.AutoSize = true;
+            this.lblGenre.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenre.Location = new System.Drawing.Point(682, 81);
+            this.lblGenre.Name = "lblGenre";
+            this.lblGenre.Size = new System.Drawing.Size(60, 22);
+            this.lblGenre.TabIndex = 8;
+            this.lblGenre.Text = "Genre";
+            // 
+            // lstBooks
+            // 
+            this.lstBooks.FormattingEnabled = true;
+            this.lstBooks.Location = new System.Drawing.Point(323, 176);
+            this.lstBooks.Name = "lstBooks";
+            this.lstBooks.Size = new System.Drawing.Size(439, 199);
+            this.lstBooks.TabIndex = 9;
+            this.lstBooks.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lstBooks_MouseDoubleClick);
+            // 
+            // btnAddBook
+            // 
+            this.btnAddBook.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddBook.Location = new System.Drawing.Point(125, 176);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(163, 34);
+            this.btnAddBook.TabIndex = 10;
+            this.btnAddBook.Text = "Add Book";
+            this.btnAddBook.UseVisualStyleBackColor = true;
+            this.btnAddBook.Click += new System.EventHandler(this.btnAddBook_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(125, 283);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(163, 36);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete Book";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddBook);
+            this.Controls.Add(this.lstBooks);
+            this.Controls.Add(this.lblGenre);
+            this.Controls.Add(this.lblYearPublished);
+            this.Controls.Add(this.lblAuthor);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.txtYearPublished);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.btnLoadBooks);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadBooks;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtYearPublished;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.Label lblYearPublished;
+        private System.Windows.Forms.Label lblGenre;
+        private System.Windows.Forms.ListBox lstBooks;
+        private System.Windows.Forms.Button btnAddBook;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ WindowsAppDemo/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsAppDemo.Models;
+
+namespace WindowsAppDemo
+{
+    public partial class Form1 : Form
+    {
+        List<Book> Freshbooks = Database.PopulateBooks();
+        
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void btnLoadBooks_Click(object sender, EventArgs e)
+        {
+            List<Book> books = Database.PopulateBooks();
+            lstBooks.DataSource = books;
+        }
+
+        private void lstBooks_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Book selectedbook = (Book)lstBooks.SelectedItem;
+            txtTitle.Text = selectedbook.Title;
+            txtAuthor.Text = selectedbook.Author;
+            txtYearPublished.Text = selectedbook.YearPublished.ToString();
+            txtGenre.Text = selectedbook.Genre;
+        }
+
+        private void btnAddBook_Click(object sender, EventArgs e)
+        {
+            Book book = new Book();
+            book.Title = txtTitle.Text;
+            book.Author = txtAuthor.Text;
+            book.YearPublished = int.Parse(txtYearPublished.Text);
+            book.Genre = txtGenre.Text;
+            
+
+            lstBooks.DataSource = null;
+            List<Book> books = Database.PopulateBooks();
+            Database.Books.Add(book);
+            lstBooks.DataSource = books;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Book selectedbook = (Book)lstBooks.SelectedItem;
+            lstBooks.DataSource = null;
+            Freshbooks.Remove(selectedbook);
+            
+            lstBooks.DataSource = Freshbooks;
+
+
+
+
+
+        }
+    }
+}
